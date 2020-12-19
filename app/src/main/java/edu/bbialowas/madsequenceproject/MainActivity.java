ecdcf980ecfb86882b7ce3605da5ef11efbb6427
Bartlomiej Bialowas <46888832+GenericUsername337@users.noreply.github.com>
2020-12-19 20:57:21 +0000

    first page done

--- app/src/main/java/edu/bbialowas/madsequenceproject/MainActivity.java
+++ app/src/main/java/edu/bbialowas/madsequenceproject/MainActivity.java
@@ -2,19 +2,34 @@ package edu.bbialowas.madsequenceproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    private final int BLUE = 1;
+    private final int RED = 2;
+    private final int GREY = 3;
+    private final int GREEN = 4;
+
     TextView round, score;
     Button bRed, bBlue, bWhite, bGreen, fb;
+
     int sequenceCount = 4, n = 0;
     private Object mutex = new Object();
+
+    int[] gameSequence = new int[120];
+    int arrayIndex = 0;
+
+    Boolean played = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,11 +44,18 @@ public class MainActivity extends AppCompatActivity
         bBlue = findViewById(R.id.btnBlue);
         bWhite = findViewById(R.id.btnGrey);
         bGreen = findViewById(R.id.btnGreen);
+
+        round.setText(String.valueOf(1));
+        score.setText(String.valueOf(0));
     }
 
     public void doPlay(View view)
     {
-        ct.start();
+        if(!played)
+        {
+            ct.start();
+            played = true;
+        }
     }
 
     public void doFinish(View view)
@@ -43,7 +65,8 @@ public class MainActivity extends AppCompatActivity
 
     public void doReset(View view)
     {
-
+        round.setText(String.valueOf(1));
+        score.setText(String.valueOf(0));
     }
 
     CountDownTimer ct = new CountDownTimer(6000, 1500) {
@@ -51,35 +74,53 @@ public class MainActivity extends AppCompatActivity
         public void onTick(long millisUntilFinished)
         {
             //mTextField.setText("seconds remaining: " + millisUntilFinished / 1500);
-            oneButton();
+            getRandomButton();
             //here you can have your logic to set text to edittext
         }
 
         public void onFinish()
         {
+            played = false;
             //mTextField.setText("done!");
+            // we now have the game sequence
+
+            for (int i = 0; i< arrayIndex; i++)
+                Log.d("game sequence", String.valueOf(gameSequence[i]));
+            // start next activity
+
+            // put the sequence into the next activity
+            // stack overglow https://stackoverflow.com/questions/3848148/sending-arrays-with-intent-putextra
+            //Intent i = new Intent(A.this, B.class);
+            //i.putExtra("numbers", array);
+            //startActivity(i);
+
+            // start the next activity
+            // int[] arrayB = extras.getIntArray("numbers");
         }
     };
 
-    private void oneButton()
+    private void getRandomButton()
     {
         n = getRandom(sequenceCount);
-
         //Toast.makeText(this, "Number = " + n, Toast.LENGTH_SHORT).show();
 
         switch (n)
         {
             case 1:
                 flashButton(bBlue);
+                gameSequence[arrayIndex++] = BLUE;
                 break;
             case 2:
                 flashButton(bRed);
+                gameSequence[arrayIndex++] = RED;
                 break;
             case 3:
                 flashButton(bWhite);
+                gameSequence[arrayIndex++] = GREY;
                 break;
             case 4:
                 flashButton(bGreen);
+                gameSequence[arrayIndex++] = GREEN;
                 break;
             default:
                 break;
@@ -115,23 +156,5 @@ public class MainActivity extends AppCompatActivity
             } // end runnable
         };
         handler.postDelayed(r, 600);
-    }
-
-    public void doTest(View view) {
-        for (int i = 0; i < sequenceCount; i++) {
-            int x = getRandom(sequenceCount);
-
-            //Toast.makeText(this, "Number = " + String.valueOf(x), Toast.LENGTH_SHORT).show();
-            //Toast.makeText(this, "Number = " + x, Toast.LENGTH_SHORT).show();
-
-            if (x == 1)
-                flashButton(bBlue);
-            else if (x == 2)
-                flashButton(bRed);
-            else if (x == 3)
-                flashButton(bWhite);
-            else if (x == 4)
-                flashButton(bGreen);
-        }
     }
 }
