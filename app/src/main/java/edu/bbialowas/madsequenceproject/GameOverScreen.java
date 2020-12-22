322d62c288c27e8a7007f6900675793575a3df67
Bartlomiej Bialowas <46888832+GenericUsername337@users.noreply.github.com>
2020-12-22 16:16:22 +0000

    made all screen layouts

---
+++ app/src/main/java/edu/bbialowas/madsequenceproject/GameOverScreen.java
@@ -0,0 +1,28 @@
+package edu.bbialowas.madsequenceproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class GameOverScreen extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over_screen);
+    }
+
+    public void doHighScores(View view)
+    {
+
+    }
+
+    public void doRestart(View view)
+    {
+
+    }
+}
---
+++ app/src/main/java/edu/bbialowas/madsequenceproject/HighScoresScreen.java
@@ -0,0 +1,14 @@
+package edu.bbialowas.madsequenceproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HighScoresScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_scores_screen);
+    }
+}
